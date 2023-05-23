53dc3c9544782b04168407face446ca57afec365
thinpv <thindcna@gmail.com>
2023-05-23 22:06:41 +0700

    update code

--- packages/matter/build.sh
+++ packages/matter/build.sh
@@ -1,46 +1,63 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/project-chip/connectedhomeip
-TERMUX_PKG_DESCRIPTION="Matter"
-TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_MAINTAINER="CSA"
-TERMUX_PKG_VERSION=SVE_23_03-rc2
-TERMUX_PKG_SRCURL=git+https://github.com/project-chip/connectedhomeip
-TERMUX_PKG_GIT_BRANCH=master
-#TERMUX_PKG_SRCURL=https://github.com/project-chip/connectedhomeip/archive/refs/tags/SVE_23_03/rc2.tar.gz
-#TERMUX_PKG_SHA256=290ecf1781aadd70ec837436bc25a1855f86beea752c3991dbf173bc475babf6
-TERMUX_PKG_DEPENDS="glib"
+TERMUX_PKG_HOMEPAGE=rangdong.com.vn
+TERMUX_PKG_DESCRIPTION="Ralli HC"
+TERMUX_PKG_LICENSE="Apache-2.0, MIT"
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_DEPENDS="glib, libc++, libcrypt, openssl, ndk-multilib, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-# termux_step_post_get_source() {
-# 	echo "termux_step_post_get_source start"
-# 	git submodule update --init
-# 	ls
-# 	pwd
-# 	echo "termux_step_post_get_source end"
-# }
-
-termux_step_pre_configure() {
-	echo "termux_step_pre_configure start"
-	source ./scripts/bootstrap.sh
-	echo "termux_step_pre_configure end"
-}
+# TERMUX_CONTINUE_BUILD=true
 
 termux_step_configure() {
 	echo "termux_step_configure start"
-	pwd
-	source scripts/activate.sh
+	if [ ! -d "$TERMUX_PKG_BUILDER_DIR/connectedhomeip" ]; then
+		echo "git clone --recurse-submodules https://github.com/project-chip/connectedhomeip.git"
+		git clone --recurse-submodules https://github.com/project-chip/connectedhomeip.git $TERMUX_PKG_BUILDER_DIR/connectedhomeip
+	fi
+	if [ ! -d "$TERMUX_PKG_BUILDDIR/connectedhomeip" ]; then
+		# ln -s $TERMUX_PKG_BUILDER_DIR/connectedhomeip $TERMUX_PKG_BUILDDIR/connectedhomeip
+		cp -a $TERMUX_PKG_BUILDER_DIR/connectedhomeip $TERMUX_PKG_BUILDDIR
+	fi
 	echo "termux_step_configure end"
 }
 
 termux_step_make() {
-	echo "termux_step_make start"
-	pwd
-	termux_setup_gn
-	gn gen out/host
-	termux_setup_ninja
-	ninja -C out/host
-	echo "termux_step_make end"
-}
+	echo "CC: " $CC
+	echo "CXX: " $CXX
+	echo "CPP: " $CPP
+	echo "LD: " $LD
+	echo "CFLAGS: " $CFLAGS
+	echo "CXXFLAGS: " $CXXFLAGS
+	echo "CPPFLAGS: " $CPPFLAGS
+	echo "LDFLAGS: " $LDFLAGS
+	echo "PKG_CONFIG: " $PKG_CONFIG
+	echo "AR: " $AR
+	echo "RANLIB: " $RANLIB
 
-termux_step_make_install() {
-	install -Dm755 -t $TERMUX_PREFIX/bin out/gn
+	echo "termux_step_make start"
+	cd connectedhomeip/examples/bridge-app/linux
+	pwd
+	# cd /home/builder/.termux-build/matter/src/connectedhomeip/examples/lighting-app/linux
+	# source third_party/connectedhomeip/scripts/activate.sh
+	# ./third_party/connectedhomeip/scripts/activate.sh
+
+	# export CC=$CC
+	# export CXX=$CXX
+	# export CPP=$CPP
+	# export CFLAGS+=$CFLAGS
+	# export CXXFLAGS+=$CXXFLAGS
+	# export CPPFLAGS+=$CPPFLAGS
+
+	echo "gn gen out/debug start"
+	termux_setup_gn
+	# gn gen out/debug
+	# gn gen out/debug --args='is_clang=true'
+	# gn gen out/debug --args='is_clang=true target_cpu="arm"'
+	# gn gen out/debug --args='is_clang=true target_cpu="arm" target_os="linux"'
+	# gn gen out/debug --args='is_clang=true target_cpu="arm" sysroot="/home/builder/lib/android-ndk-r23c/toolchains/llvm/prebuilt/linux-x86_64/sysroot"'
+	# gn gen out/debug --args='host_toolchain="android" is_clang=true target_cpu="arm" target_os="linux" sysroot="/home/builder/lib/android-ndk-r23c/toolchains/llvm/prebuilt/linux-x86_64/sysroot"'
+	# gn gen out/debug --args='host_toolchain="custom" is_clang=true target_cpu="arm" target_os="linux" sysroot="/home/builder/lib/android-ndk-r23c/toolchains/llvm/prebuilt/linux-x86_64/sysroot"'
+	gn gen out/debug --args='target_cpu="arm" target_os="android" sysroot="/home/builder/lib/android-ndk-r23c/toolchains/llvm/prebuilt/linux-x86_64/sysroot"'
+	
+	echo "ninja -C out/debug start"
+	termux_setup_ninja
+	ninja -C out/debug
 }
